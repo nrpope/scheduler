60420b4164085a4cf37c7ead60ce1b087ee3e870
Nick Pope <psydpope@gmail.com>
2020-03-18 09:36:28 -0700

    added cancel to Form selection

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,17 @@ import InterviewerList from '../InterviewerList';
 export default function Form(props) {
   const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName('');
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -27,7 +38,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
           <Button confirm onClick={props.onSave}>
