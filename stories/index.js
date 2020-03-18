bc0291068f5453bcddd3036ceabcccbc5fc70cc3
Nick Pope <psydpope@gmail.com>
2020-03-18 09:20:47 -0700

    added components to appointment with stories

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,12 @@ import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
+import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Error from 'components/Appointment/Error';
+import Status from 'components/Appointment/Status';
 
 //button story
 
@@ -144,4 +150,25 @@ storiesOf('Appointment', module)
     backgrounds: [{ name: 'white', value: '#fff', default: true }]
   })
   .add('Appointment', () => <Appointment />)
-  .add('Appointment with Time', () => <Appointment time="12pm" />);
+  .add('Appointment with Time', () => <Appointment time="12pm" />)
+  .add('Header', () => <Header time="12pm" />)
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ))
+  .add('Confirm', () => (
+    <Confirm
+      message="Delete the appointment?"
+      onCancel={action('onCancel')}
+      onConfirm={action('onConfirm')}
+    />
+  ))
+  .add('Status', () => <Status message="Deleting" />)
+  .add('Error', () => (
+    <Error message="Could not delete appointment" onClose={action('onClose')} />
+  ));
