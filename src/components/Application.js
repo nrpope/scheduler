d4f234e38176611a25d1ec0af0f8ad07c3e3309c
Nick Pope <psydpope@gmail.com>
2020-03-18 11:11:37 -0700

    added form data to appointment mock data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
+import Appointment from 'components/Appointment';
 import 'components/Application.scss';
-import 'components/DayList';
 import DayList from 'components/DayList';
 
 const days = [
@@ -19,6 +19,53 @@ const days = [
     id: 3,
     name: 'Wednesday',
     spots: 0
+  }
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: '12pm'
+  },
+  {
+    id: 2,
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png'
+      }
+    }
+  },
+  {
+    id: 3,
+    time: '2pm'
+  },
+  {
+    id: 4,
+    time: '3pm',
+    interview: {
+      student: 'Ben Clift',
+      interviewer: {
+        id: 2,
+        name: 'Tori Palmer',
+        avatar: 'https://i.imgur.com/Nmx0Qxo.png'
+      }
+    }
+  },
+  {
+    id: 5,
+    time: '4pm',
+    interview: {
+      student: 'OmegaLUL',
+      interviewer: {
+        id: 3,
+        name: 'Mildred Nazir',
+        avatar: 'https://i.imgur.com/T2WwVfS.png'
+      }
+    }
   }
 ];
 
@@ -43,7 +90,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment key={appointment.id} {...appointment} />
+        ))}
+        <Appointment id="last" time="5pm" />
       </section>
     </main>
   );
