53943baa17ccfedf80e2c82dc5332eaeb17dc65c
Nick Pope <psydpope@gmail.com>
2020-03-23 14:53:30 -0700

    type checking with prop types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
 import 'components/InterviewerList.scss';
 import InterviewerListItem from './InterviewerListItem';
 
 export default function InterviewerList(props) {
+  InterviewerList.propTypes = {
+    value: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+  };
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
