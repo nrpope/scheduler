931ba40d0cadc98e6b9ddf8ebf5d6dce8a074584
Nick Pope <psydpope@gmail.com>
2020-03-24 14:51:05 -0700

    test data from mock axios

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,41 +1,56 @@
-export function getAppointmentsForDay(state, day) {
-  const filteredDays = state.days.filter(singleDay => {
-    return singleDay.name === day;
-  });
+export const getAppointmentsForDay = (state, day) => {
+  const dayObj = state.days.find(elem => elem.name === day);
 
-  if (filteredDays.length === 0) {
+  if (!dayObj) {
     return [];
   }
-  const appointmentsMapped = filteredDays[0].appointments.map(app => {
-    return state.appointments[app];
-  });
 
-  return appointmentsMapped;
-}
+  const appointmentIds = dayObj.appointments;
 
-export function getInterview(state, interview) {
+  const appointmentsForDay = [];
+
+  for (const id in state.appointments) {
+    if (appointmentIds.includes(Number(id))) {
+      appointmentsForDay.push(state.appointments[id]);
+    }
+  }
+
+  return appointmentsForDay;
+};
+
+export const getInterviewersForDay = (state, day) => {
+  const dayObj = state.days.find(elem => elem.name === day);
+
+  if (!dayObj) {
+    return [];
+  }
+
+  const interviewerIds = dayObj.interviewers;
+
+  const interviewersForDay = [];
+
+  for (const id in state.interviewers) {
+    if (interviewerIds.includes(Number(id))) {
+      interviewersForDay.push(state.interviewers[id]);
+    }
+  }
+
+  return interviewersForDay;
+};
+
+export const getInterview = (state, interview) => {
   if (!interview) {
     return null;
   }
-  const newInterview = {
-    ...interview,
-    interviewer: { ...state.interviewers[interview.interviewer] }
-  };
 
-  return newInterview;
-}
+  const interviewerId = interview.interviewer;
 
-export function getInterviewersForDay(state, day) {
-  const filteredDays = state.days.filter(singleDay => {
-    return singleDay.name === day;
-  });
-
-  if (filteredDays.length === 0) {
-    return [];
+  for (const id in state.interviewers) {
+    if (Number(id) === interviewerId) {
+      return {
+        student: interview.student,
+        interviewer: state.interviewers[id]
+      };
+    }
   }
-  const interviewersMapped = filteredDays[0].interviewers.map(int => {
-    return state.interviewers[int];
-  });
-
-  return interviewersMapped;
-}
+};
