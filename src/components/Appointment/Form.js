02b99ec9c7438aea1c9cc8dbdbf54b0f8d9bb7e1
Nick Pope <psydpope@gmail.com>
2020-04-09 23:04:53 +0000

    added interview validation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,14 +7,6 @@ export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [name, setName] = useState(props.name || '');
   const [error, setError] = useState('');
-  function validate() {
-    if (name === '') {
-      setError('Student name cannot be blank');
-      return;
-    }
-    setError('');
-    props.onSave(name, interviewer);
-  }
 
   const reset = () => {
     setName('');
@@ -22,21 +14,32 @@ export default function Form(props) {
   };
 
   const cancel = () => {
-    reset();
     props.onCancel();
+    reset();
   };
 
+  const validate = () => {
+    if (name === '') {
+      setError('Student name cannot be blank');
+      return;
+    } else if (interviewer === null) {
+      setError('Interviewer must be selected to proceed!');
+      return;
+    }
+    setError('');
+    props.onSave(name, interviewer);
+  };
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             value={name}
-            onChange={event => setName(event.target.value)}
+            onChange={(event) => setName(event.target.value)}
             data-testid="student-name-input"
           />
           <section className="appointment__validation">{error}</section>
